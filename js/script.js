abcd4378882bd76abf3b09aeaa69a7bcb77ae58e
Ambar-A <ambaralvarado414@gmail.com>
2020-03-03 23:28:24 +0000

    properties and values

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let 2Cool4Skool_1 = [{
+        name: "Intro: Cool 4 Skool (feat. DJ Friz)",
+        
+        
+    }];
+
+let 2Cool4Skool_2 = {
+        name: "I Like It",
+        
+    }
+
+let O!RUL8,2?_1 = {
+        name: "We On",
+        
+    }
+
+let O!RUL8,2?_2 = {
+        name: "Coffee",
+        
+    }
+
+let O!RUL8,2?_3 = {
+        name: "Attack on Bangtan",
+        
+    }
+
+let skoolLuvAffair_1 = {
+        name: "Boy In Luv",
+        
+    }
+
+let skoolLuvAffair_2 = {
+        name: "Just One Day",
+        
+    }
+
+let skoolLuvAffair_3 = {
+        name: "Tomorrow",
+        
+    }
+
+let skoolLuvAffair_4 = {
+        name: "BTS Cypher Pt. 2: Triptych",
+        
+    }
+
+let skoolLuvAffair_5 = {
+        name: "Spine Breaker",
+        
+    }
+
+let  = {
+        name:
+        
+    }
+
+let Skool Luv Affair_1 = {
+        name:
+        
+    }
+
+let Skool Luv Affair_1 = {
+        name:
+        
+    }
+let playlist = {
+
+}
