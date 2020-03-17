c47a1aeae3323c4962f1f71106153edcd08a891e
Ambar-A <ambaralvarado414@gmail.com>
2020-03-17 23:20:46 +0000

    did javascript

--- js/script.js
+++ js/script.js
@@ -1,68 +1,464 @@
-let 2Cool4Skool_1 = [{
-        name: "Intro: Cool 4 Skool (feat. DJ Friz)",
-        
-        
-    }];
-
-let 2Cool4Skool_2 = {
-        name: "I Like It",
-        
-    }
-
-let O!RUL8,2?_1 = {
-        name: "We On",
-        
-    }
-
-let O!RUL8,2?_2 = {
-        name: "Coffee",
-        
-    }
-
-let O!RUL8,2?_3 = {
-        name: "Attack on Bangtan",
-        
-    }
-
-let skoolLuvAffair_1 = {
-        name: "Boy In Luv",
-        
-    }
-
-let skoolLuvAffair_2 = {
-        name: "Just One Day",
-        
-    }
-
-let skoolLuvAffair_3 = {
-        name: "Tomorrow",
-        
-    }
-
-let skoolLuvAffair_4 = {
-        name: "BTS Cypher Pt. 2: Triptych",
-        
-    }
-
-let skoolLuvAffair_5 = {
-        name: "Spine Breaker",
-        
-    }
-
-let  = {
-        name:
-        
-    }
-
-let Skool Luv Affair_1 = {
-        name:
-        
-    }
-
-let Skool Luv Affair_1 = {
-        name:
-        
-    }
 let playlist = {
-
+song1 = {
+    artist: "BTS",
+    name: "Intro: 2 Cool 4 Skool (feat. JD Friz)",
+    album: "2 Cool 4 Skool",
+    time: 
 }
+
+song2 = {
+    artist: "BTS",
+    name: "We On",
+    album: "O!RUL82?",
+    time: 
+}
+
+song3 = {
+artist: "BTS",
+name: "Coffee",
+album: "O!RUL82?",
+time: 
+}
+
+song4 = {
+        artist: "BTS",
+        name: "Attack On Bangtan",
+        album: "O!RUL82?",
+        time: 
+}
+
+song5 = {
+        artist: "BTS",
+        name: "Boy In Luv",
+        album: "Skool Luv Affair",
+        time: 
+}
+
+song6 = {
+        artist: "BTS",
+        name: "Just One Day",
+        album: "Skool Luv Affair",
+        time: 4:00,
+}
+
+song7 = {
+        artist: "BTS",
+        name: "Tomorrow",
+        album: "Skool Luv Affair",
+        time: 4:22,
+}
+
+song8 = {
+        artist: "BTS",
+        name: "Cypher Pt. 2: Triptych",
+        album: "Skool Luv Affair",
+        time: 4:49,    
+}
+
+song9 = {
+        artist: "BTS",
+        name: "Spine Breaker",
+        album: "Skool Luv Affair",
+        time: 3:59,    
+}
+
+song10 = {
+        artist: "BTS",
+        name: "War of Hormone",
+        album: "Dark & Wild",
+        time: 4:26,    
+}
+
+song11 = {
+        artist: "BTS",
+        name: "Hip Hop Phile",
+        album: "Dark & Wild",
+        time: 4:17,    
+}
+
+song12 = {
+        artist: "BTS",
+        name: "Let Me Know",
+        album: "Dark & Wild",
+        time: 4:16,    
+}
+
+song13 = {
+        artist: "BTS",
+        name: "Rain",
+        album: "Dark & Wild",
+        time: 4:25,
+}
+
+song14 = {
+        artist: "BTS",
+        name: "Cypher Pt. 3: KILLER (feat. Supreme Boi)",
+        album: "Dark & Wild",
+        time: 4:28,   
+}
+
+song15 = {
+        artist: "BTS",
+        name: "Look Here",
+        album: "Dark & Wild",
+        time: 
+}
+
+song16 = {
+        artist: "BTS",
+        name: "Second Grade",
+        album: "Dark & Wild",
+        time:   
+}
+
+song17 = {
+        artist: "BTS",
+        name: "Dope",
+        album: "The Most Beautiful Moment in Life Part 1",
+        time: 
+}
+
+song18 = {
+        artist: "BTS",
+        name: "Boyz With Fun",
+        album: "The Most Beautiful Moment in Life Part 1",
+        time: 
+}
+
+song19 = {
+        artist: "BTS",
+        name: "Ma City",
+        album: "The Most Beautiful Moment in Life Part 2",
+        time: 
+}
+
+song20 = {
+        artist: "BTS",
+        name: "Baepsae",
+        album: "The Most Beautiful Moment in Life Part 2",
+        time:    
+}
+
+song21 = {
+        artist: "BTS",
+        name: "Fire",
+        album: "The Most Beautiful Moment in Life: Young Forever",
+        time:  
+}
+
+song22 = {
+        artist: "BTS",
+        name: "Intro: Boy Meets Evil",
+        album: "Wings",
+        time: 
+}
+
+song23 = {
+        artist: "BTS",
+        name: "Blood Sweat & Tears",
+        album: "Wings",
+        time:    
+}
+
+song24 = {
+        artist: "BTS",
+        name: "Begin",
+        album: "Wings",
+        time:  
+}
+
+song25 = {
+        artist: "BTS",
+        name: "Lie",
+        album: "Wings",
+        time: 
+}
+
+song26 = {
+        artist: "BTS",
+        name: "Stigma",
+        album: "Wings",
+        time: 
+}
+
+song27 = {
+        artist: "BTS",
+        name: "First Love",
+        album: "Wings",
+        time:  
+}
+
+song28 = {
+        artist: "BTS",
+        name: "Reflection",
+        album: "Wings",
+        time: 
+}
+
+song29 = {
+        artist: "BTS",
+        name: "MAMA",
+        album: "Wings",
+        time: 
+}
+
+song30 = {
+        artist: "BTS",
+        name: "Awake",
+        album: "Wings",
+        time: 
+}
+
+song31 = {
+        artist: "BTS",
+        name: "Lost",
+        album: "Wings",
+        time:   
+}
+
+song32 = {
+        artist: "BTS",
+        name: "Cypher 4",
+        album: "Wings",
+        time: 
+}
+
+song33 = {
+        artist: "BTS",
+        name: "Am I Wrong",
+        album: "Wings",
+        time: 
+}
+
+song34 = {
+        artist: "BTS",
+        name: "21st Century Girl",
+        album: "Wings",
+        time: 
+}
+
+song35 = {
+        artist: "BTS",
+        name: "Wings",
+        album: "Wings",
+        time: 
+}
+
+song36 = {
+        artist: "BTS",
+        name: "Not Today",
+        album: "You Never Walk Alone",
+        time: 
+}
+
+song37 = {
+        artist: "BTS",
+        name: "Intro: Serendipity",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song38 = {
+        artist: "BTS",
+        name: "DNA",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song39 = {
+        artist: "BTS",
+        name: "Best Of Me",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song40 = {
+        artist: "BTS",
+        name: "Dimple",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song41 = {
+        artist: "BTS"
+        name: "Mic Drop (Steve Aoki Remix)",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song42 = {
+        artist: "BTS",
+        name: "Go Go",
+        album: "Love Yourself: Her",
+        time: 
+}
+
+song43 = {
+        artist: "BTS",
+        name: "Intro: Singularity",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song44 = {
+        artist: "BTS",
+        name: "Fake Love",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song45 = {
+        artist: "BTS",
+        name: "Pluto",
+        album: "Love Yourself: Tear",
+        time:  
+}
+
+song46 = {
+        artist: "BTS",
+        name: "Paradise",
+        album: "Love Yourself: Tear",
+        time:   
+}
+
+song47 = {
+        artist: "BTS",
+        name: "Love Maze",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song48 = {
+        artist: "BTS",
+        name: "Magic Shop",
+        album: "Love Yourself: Tear",
+        time:  
+}
+
+song49 = {
+        artist: "BTS",
+        name: "Airplaine Pt. 2",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song50 = {
+        artist: "BTS",
+        name: "Anpanman",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song51 = {
+        artist: "BTS",
+        name: "Airplaine Pt. 2",
+        album: "Love Yourself: Tear",
+        time:   
+}
+
+song52 = {
+        artist: "BTS",
+        name: "So What",
+        album: "Love Yourself: Tear",
+        time: 
+}
+
+song53 = {
+        artist: "BTS",
+        name: "Outro: Tear",
+        album: "Love Yourself: Tear",
+        time:  
+}
+
+song54 = {
+        artist: "BTS",
+        name: "Euphoria",
+        album: "Love Yourself: Answer",
+        time:  
+}
+
+song55 = {
+        artist: "BTS",
+        name: "Trivia: Just Dance",
+        album: "Love Yourself: Answer",
+        time: 
+}
+
+song56 = {
+        artist: "BTS",
+        name: "Trivia: Love",
+        album: "Love Yourself: Answer",
+        time: 
+}
+
+song57 = {
+        artist: "BTS",
+        name: "Trivia: Seesaw",
+        album: "Love Yourself: Answer",
+        time: 
+}
+
+song58 = {
+        artist: "BTS",
+        name: "Epiphany",
+        album: "Love Yourself: Answer",
+        time:    
+}
+
+song59 = {
+        artist: "BTS",
+        name: "IDOL (feat. Nicki Minaj)",
+        album: "Love Yourself: Answer",
+        time:  
+}
+
+song60 = {
+        artist: "BTS",
+    ame: "Intro: Persona",
+        album: "Map of the Soul: Persona",
+        time: 
+}
+
+song61 = {
+        artist: "BTS",
+        name: "Boy With Luv (feat. Halsey)",
+        album: "Map of the Soul: Persona",
+        time: 
+}
+
+song 62 = {
+        artist: "BTS"
+        name: "Mikrokosmos",
+        album: "Map of the Soul: Persona",
+        time: 
+}
+
+song63 = {
+        artist: "BTS",
+        name: "Make It Right",
+        album: "Map of the Soul: Persona",
+        time:    
+}
+
+song64 = {
+        artist: "BTS",
+        name: "HOME",
+        album: "Map of the Soul: Persona",
+        time: 
+}
+
+song65 = {
+        artist: "BTS",
+        name: "Jamais Vu",
+        album: "Map of the Soul: Persona",
+        time:   
+}
+
+song66 = {
+        artist: "BTS",
+        name: "Dionysus",
+        album: "Map of the Soul: Persona",
+        time: 
+}
+};
+
